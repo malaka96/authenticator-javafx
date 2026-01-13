756f4342b7d7d4752bcfce14868513b7a920b364
malaka96 <madhubhashana655@gmail.com>
2026-01-13 09:58:21 +0530

    completed ui

--- src/main/java/controller/DashboardController.java
+++ src/main/java/controller/DashboardController.java
@@ -1,5 +1,72 @@
 package controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+
 public class DashboardController {
+
+    @FXML
+    private TextField CreateFirstNameTf;
+
+    @FXML
+    private TextField CreateLastNameTf;
+
+    @FXML
+    private PasswordField CreatePasswordTf;
+
+    @FXML
+    private PasswordField CreateReEnterPasswordTf;
+
+    @FXML
+    private AnchorPane createAccPane;
+
+    @FXML
+    private AnchorPane dashboardPane;
+
+    @FXML
+    private TextField loginEmailTf;
+
+    @FXML
+    private AnchorPane loginPane;
+
+    @FXML
+    private PasswordField loginPsTf;
+
+    @FXML
+    void CreateBtnAction(ActionEvent event) {
+        showTargetScreen(dashboardPane);
+    }
+
+    @FXML
+    void createLoginBtnAction(MouseEvent event) {
+        showTargetScreen(loginPane);
+    }
+
+    @FXML
+    void dashboradLogoutBtnAction(ActionEvent event) {
+        showTargetScreen(loginPane);
+    }
+
+    @FXML
+    void loginAccCreateBtnAction(MouseEvent event) {
+        showTargetScreen(createAccPane);
+    }
+
+    @FXML
+    void loginBtnAction(ActionEvent event) {
+        showTargetScreen(dashboardPane);
+    }
+
+    private void showTargetScreen(AnchorPane targetAnchorPane){
+        loginPane.setVisible(false);
+        createAccPane.setVisible(false);
+        dashboardPane.setVisible(false);
+
+        targetAnchorPane.setVisible(true);
+    }
 
 }
