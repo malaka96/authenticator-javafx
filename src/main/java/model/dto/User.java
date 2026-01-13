b6d13d64dbb556dda37568152004443c55fab5a2
malaka96 <madhubhashana655@gmail.com>
2026-01-13 10:25:31 +0530

    created user model

---
+++ src/main/java/model/dto/User.java
@@ -0,0 +1,17 @@
+package model.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+public class User {
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+}
